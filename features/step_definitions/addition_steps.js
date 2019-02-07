a9338640944c10261f8956cdba9ba6317550ccd8
hatem-hadrich <haptou28@gmail.com>
2019-02-07 01:31:43 +0100

    Add cucumber js

---
+++ features/step_definitions/addition_steps.js
@@ -0,0 +1,15 @@
+const { Before, Given, When, Then } = require('cucumber');
+const Calculator = require('../../lib/calculator');
+
+let calculator;
+
+Given('the numbers {int} and {int}', function (x, y) {
+    calculator = new Calculator(x, y);
+});
+
+When('they are added together', function () {
+    calculator.add();
+});
+
+Then('should the result be {int}', function (int) {
+});
